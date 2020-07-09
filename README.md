b270eef4482004ed8de2e7bfc41d7194744406c9
john <john6cc@hotmail.com>
2020-07-09 16:29:08 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# TuDu_prototype
+ 
---
+++ TuDu_prototype/main/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+from .forms import UserLoginForm
+
+app_name = 'main'
+
+
+urlpatterns = [
+
+    path('', views.home, name='home'),
+    path('logout/', views.user_logout, name='logout'),
+    path('login/', views.user_login, name='login'),
+    path('register/', views.register, name='register'),
+
+]
