ca5869c74abd90e6ed2a6e94c8930fba6ab8139a
john <john6cc@hotmail.com>
2020-07-10 14:13:06 +0100

    model changes 10/07 JOHN

--- main/models.py
+++ main/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils import timezone
+from datetime import datetime as dt
 from mapbox_location_field.models import LocationField
 from django.contrib.auth.models import User
 import uuid
@@ -15,7 +15,7 @@ class Categories(models.Model):
     
     name = models.CharField(max_length=200, primary_key=True)
     summary = models.TextField()
-    upload_date = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(default=dt.now())
 
     class Meta:
         verbose_name_plural = 'Categories'
@@ -23,23 +23,37 @@ class Categories(models.Model):
     def _str_(self):
         return self.name
 
+    
+class SubCategories(models.Model):
+
+    name = models.CharField(max_length=200, primary_key=True)
+    category = models.ForeignKey(Categories, default='Unknown', on_delete=models.SET_DEFAULT)
+    summary = models.TextField()
+    created = models.DateTimeField(default=dt.now())
+
+    class Meta:
+        verbose_name_plural = 'Sub-Categories'
+
+    def _str_(self):
+        return self.name
+
 
 class Locations(models.Model):
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    category = models.ForeignKey(Categories, default='Unknown', on_delete=models.SET_DEFAULT)
+    sub_categories = models.ManyToManyField(SubCategories)
     name = models.CharField(max_length=200, unique=True)
     location = LocationField()
     nearest_access = LocationField()
     description = models.TextField()
     photo = models.ImageField(upload_to='photos/')
-    upload_date = models.DateTimeField(default=timezone.now)
+    upload_date = models.DateTimeField(default=dt.now())
 
     class Meta:
         verbose_name_plural = 'Locations'
 
     def _str_(self):
-        return self.category, self.name
+        return self.name
 
 
 class Visits(models.Model):
@@ -52,7 +66,7 @@ class Visits(models.Model):
     Rating_CHOICES = ((1, 'Poor'), (2, 'Average'), (3, 'Good'), (4, 'Very Good'), (5, 'Excellent'))
     rating = models.IntegerField(choices=Rating_CHOICES, default=3)
     is_fraudlent = models.BooleanField(default=False)
-    visit_date = models.DateTimeField(default=timezone.now)
+    visit_date = models.DateTimeField(default=dt.now())
 
     class Meta:
         verbose_name_plural = 'Visits'
