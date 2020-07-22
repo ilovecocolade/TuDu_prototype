6cea6a963f21e87294b422055f04b1204aa8ce71
ilovecocolade <41858339+ilovecocolade@users.noreply.github.com>
2020-07-22 12:13:26 +0100

    pull

--- main/models.py
+++ main/models.py
@@ -3,6 +3,7 @@ from datetime import datetime as dt
 from mapbox_location_field.models import LocationField
 from django.contrib.auth.models import User
 import uuid
+from django.conf import settings
 
 
 # Create your models here.
@@ -39,7 +40,7 @@ class SubCategories(models.Model):
 class Locations(models.Model):
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    uploader = models.ForeignKey(User, default='Unknown', on_delete=models.SET_DEFAULT)
+    #uploader = models.ForeignKey(settings.AUTH_USER_MODEL, default='Unknown', on_delete=models.SET_DEFAULT)
     primary_sub_category = models.ForeignKey(SubCategories, default='Unknown', on_delete=models.SET_DEFAULT)
     secondary_sub_categories = models.ManyToManyField(SubCategories, related_name='secondary_sub_categories')
     name = models.CharField(max_length=200, unique=True)
